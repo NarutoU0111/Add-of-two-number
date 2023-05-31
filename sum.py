42881a7f15ddc5065f78871240580845d182c636
NarutoU0111 <132562290+NarutoU0111@users.noreply.github.com>
2023-05-31 12:33:06 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,12 @@
+import os
+def add(a,b):
+    c=a+b
+
+    #return c
+
+    print("Addition of Two number is :",c)
+num1=6
+
+num2=6 
+
+add(num1,num2)
---
+++ test_add.py
@@ -0,0 +1,5 @@
+from sum import *
+import pytest
+
+def test_add():
+    assert add(6,6)==12
